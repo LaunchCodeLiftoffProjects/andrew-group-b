ff31a4a190a52e267a2de68bd90a9a8f9b8c281b
rlp390 <rlp390@hotmail.com>
2021-08-16 19:30:25 -0500

    Added landing page and fragments for Navbar, Header and Footer

---
+++ ouchy/src/main/java/org/launchcode/ouchy/models/Service.java
@@ -0,0 +1,27 @@
+package org.launchcode.ouchy.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Service {
+
+    public Service() {}
+
+    @Id
+    @GeneratedValue
+    private int serviceId;
+
+    @ManyToOne
+    private int providerId;
+
+    @NotNull
+    private double serviceCost;
+
+    @NotBlank
+    private String serviceName;
+}
