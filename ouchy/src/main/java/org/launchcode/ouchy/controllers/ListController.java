59899cc26629a662028c3a882552bc189de2665d
rlp390 <rlp390@hotmail.com>
2021-08-25 16:39:21 -0500

    Added autocomplete to search field.  Autocomplete searches service names and provider names.

--- ouchy/src/main/java/org/launchcode/ouchy/controllers/ListController.java
+++ ouchy/src/main/java/org/launchcode/ouchy/controllers/ListController.java
@@ -4,12 +4,15 @@ import org.launchcode.ouchy.models.Data.ProviderRepository;
 import org.launchcode.ouchy.models.Data.ProviderServiceDTORepository;
 import org.launchcode.ouchy.models.Data.ServiceRepository;
 import org.launchcode.ouchy.models.Provider;
+import org.launchcode.ouchy.models.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ListController {
@@ -53,5 +56,31 @@ public class ListController {
         model.addAttribute("title", "Our Mission");
 
         return "mission";
+    }
+
+    @RequestMapping("searchTermAutocomplete")
+    @ResponseBody
+    public List<String> searchTermAutocomplete(@RequestParam String term) {
+        List<String> suggestions = new ArrayList<String>();
+
+        Iterable<Provider> providers = providerRepository.findAll();
+        Iterable<Service> services = serviceRepository.findAll();
+
+        System.out.println("Search Term = " + term);
+
+        for(Provider provider : providers) {
+            if(provider.getProviderName().toLowerCase().startsWith(term.toLowerCase())) {
+                suggestions.add(provider.getProviderName());
+                System.out.println("Suggestion Added:  " + provider.getProviderName());
+            }
+        }
+
+        for(Service service : services) {
+            if(service.getServiceName().toLowerCase().startsWith(term.toLowerCase())) {
+                suggestions.add(service.getServiceName());
+            }
+        }
+
+        return suggestions;
     }
 }
