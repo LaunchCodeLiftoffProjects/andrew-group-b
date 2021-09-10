98323ed6cfebe09335ef8adf5c0fc2155f82a7f8
rlp390 <rlp390@hotmail.com>
2021-09-09 19:34:27 -0500

    Created sort functions for the service list.  Added buttons to service list so user can quickly sort by name Ascending or Descending.

--- ouchy/src/main/java/org/launchcode/ouchy/models/Data/ServiceRepository.java
+++ ouchy/src/main/java/org/launchcode/ouchy/models/Data/ServiceRepository.java
@@ -1,9 +1,13 @@
 package org.launchcode.ouchy.models.Data;
 
+import org.launchcode.ouchy.models.Provider;
 import org.launchcode.ouchy.models.Service;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServiceRepository extends PagingAndSortingRepository<Service, Integer> {
+    Iterable<Service> findByOrderByServiceNameAsc();
+
+    Iterable<Service> findByOrderByServiceNameDesc();
 }
